2488f2b103106cd1c1966e308503a05029364974
farrosaji <115626685+farrosaji@users.noreply.github.com>
2022-11-04 02:41:26 +0700

    Add files via upload

--- MODUL2 FARROS/form booking.php
+++ MODUL2 FARROS/form booking.php
@@ -5,30 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form BOOKING</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style type="text/css">
+        a{text-decoration: none; font-size: 20px;font-family: sans-serif;padding: 14px 10px}
+        ul{padding: 14px}
+        li{list-style: none; display: inline;}
+        li a{background: #222; color:#d4d4d4;}
+        .navi{background: #222; height: 50px}
+        h2{font-size: small;}
+        </style>
 </head>
 <body>
-    <div class="conteiner">
-        <h1 class="text-center">Rent Your Car Now!</h1>
-        <form action="booking.php" methode="POST">
+  <div class="conteiner">
+  <h1 class="text-center">Rent Your Car Now!</h1>
+    <form action="output.php" methode="GET">
         <div class="col mb-3">
         <br>
             <label for="Nama">Name</label>
-            <input type="text" class="form-control bg-light" id="Name" readonly="readonly" value="" name="Name" required>
+            <input type="text" class="form-control " id="Name" value="" name="Name" required>
           </div>
-
           <div class="mb-3">
             <label for="Date">Date</label>
             <input type="date" class="form-control" id="Date" name="Date" required>
           </div>
-
           <div class="mb-3">
             <label for="Start">Start Time</label>
             <input type="time" class="form-control" id="Start" name="Start" required>
           </div>
-
           <div class="mb-3">
             <label for="Duration">Duration</label>
             <input type="number" class="form-control" id="Duration" name="Duration" required>
+          </div>
+          <div class="mb-3">
+            <label for="phone">No Handphone</label>
+            <input type="text" class="form-control" id="phone" name="phone" required>
           </div>
           <div class="mb-3">
             <label for="Type">Car Type</label>
@@ -36,12 +46,8 @@
             <option value="Toyota Rush">Toyota Vios</option>
             <option value="Toyota Ayla">Toyota Avanza</option>
             <option value="Honda Brio">Toyota Agya</option>
-
-          <div class="mb-3">
-            <label for="Phone">Phone Number</label>
-            <input type="number" class="form-control" id="Phone" name="Phone" required>
           </div>
-
+          
           <div class="mb-3">
             <li class="m-1">
               <label for="service">Add Service</label><br>
@@ -54,7 +60,16 @@
           <div class="mb-3 d-grid gap-2">
             <button class="btn btn-primary btn-block" type="submit">Book</button>
           </div>
-        </form>
+      </form>
+
+  </div>
+    
+       
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+<footer align="center">
+    <div class="outer-footer">Copyright 2022 (HAIDAR FARROS_1202200125_SI4401)</div>
+</footer>
 
     </div>
 </body>
--- MODUL2 FARROS/output.php
+++ MODUL2 FARROS/output.php
@@ -5,17 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>booking</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style type="text/css">
+        a{text-decoration: none; font-size: 20px;font-family: sans-serif;padding: 14px 10px}
+        ul{padding: 14px}
+        li{list-style: none; display: inline;}
+        li a{background: #222; color:#d4d4d4;}
+        .navi{background: #222; height: 50px}
+        h2{font-size: small;}
+        </style>
 </head>
 <body>
     <?php
-    $bookingnumber = $_GET['bookingnumber'];
-    $name = $_GET['name'];
-    $checkin= $_GET['checkin'];
-    $checkout= $_GET['checkout'];
-    $cartype= $_POST['cartype'];
-    $phonenumber= $_GET['phonenumber'];
-    $service= $_GET['service'];
-    $totalprice= $_GET['totalprice'];
+    $Name = $_GET['Name'];
+    $Date = $_GET['Date'];
+    $Start= $_GET['Start'];
+    $Duration= $_GET['Duration'];
+    $phone= $_GET['phone'];
+    
     ?>
         <div class="container">
             <h1 class="text-center">Thank you FARROS_1202200125 for reserving</h1>
@@ -23,35 +30,39 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">booking number</th>
+                        <th scope="col">no</th>
                         <th scope="col">name</th>
-                        <th scope="col">check in</th>
-                        <th scope="col">check out</th>
-                        <th scope="col">car type</th>
-                        <th scope="col">phone number</th>
-                        <th scope="col">service</th>
-                        <th scope="col">total price</th>
+                        <th scope="col">date</th>
+                        <th scope="col">start time</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">no Handphone</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td><?php echo $bookingnumber ?></td>
-                        <td><?php echo $name ?></td>
-                        <td><?php echo $checkin ?></td>
-                        <td><?php echo $checkout ?></td>
-                        <td><?php echo $cartype ?></td>
-                        <td><?php echo $phonenumber ?></td>
-                        <td><?php echo $service ?></td>
-                        <td><?php echo $totalprice ?></td>
+                        <td><?php echo $Name ?></td>
+                        <td><?php echo $Date ?></td>
+                        <td><?php echo $Start ?></td>
+                        <td><?php echo $Duration ?></td>
+                        <td><?php echo $phone?></td>
+                        
+                        
                     </tr>
                 </tbody>
             </table>
             <div class="mb-3">
                 <div class="d-flex justify-content-center">
-                    <a href="from booking.php" class="btn btn-outline-primary w-50">Kembali</a>
+                    <a href="home.php" class="btn btn-outline-primary w-50">Kembali</a>
                 </div>
             </div>
         </div>
+         
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+<footer align="center">
+    <div class="outer-footer">Copyright 2022 (HAIDAR FARROS_1202200125_SI4401)</div>
+</footer>
 </body>
 </html>
