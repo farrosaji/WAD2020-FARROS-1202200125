2493e53f3996fc3a494bc37a6fc9cd5b6f7b4253
farrosaji <115626685+farrosaji@users.noreply.github.com>
2022-12-03 19:37:43 +0700

    Add files via upload

---
+++ MODUL4 FARROS/config/connector.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost:3307";
+$user = "root";
+$pass = "";
+$db = "modul4";
+
+$koneksi = new mysqli($host, $user, $pass, $db);
---
+++ MODUL4 FARROS/config/logout.php
@@ -0,0 +1,10 @@
+    <?php
+    session_start();
+
+
+    header("location:../index.php");
+    // delete cookie
+    setcookie('id', '', time() - 3600);
+    setcookie('key', '', time() - 3600);
+
+    exit;
