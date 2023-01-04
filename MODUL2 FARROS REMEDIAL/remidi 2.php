4a6f434ab6a1bd65e2c3c411eeaa23b19aa52a06
farrosaji <115626685+farrosaji@users.noreply.github.com>
2023-01-04 17:36:22 +0700

    Add files via upload

---
+++ MODUL2 FARROS REMEDIAL/remidi 2.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+</head>
+<body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <nav class="navbar navbar-expand-lg bg-light" style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#"><img src="foto4.jfif" width="100" height="50" ></a>
+              <a class="nav-link" href="#Profil Kami">Profil Kami</a>
+              <a class="nav-link" href="#tempat">Lokasi Kami</a>
+              <a class="nav-link" href="#Jasa Kami">Jasa Kami</a>
+              <a class="nav-link" href="login.html">ADMIN</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+ <br><br><br>
+  <br><br><br>
+<center>
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="foto1.jfif" class="d-block w-85" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="foto6.jfif" class="d-block w-85" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="foto2.jfif" class="d-block w-85" alt="...">
+      </div>
+    </div>
+  </div>
+</center>
+
+<br><br><br>
+  <br><br><br>
+
+  <section id="tempat" class="tempat">
+    <div class="tempat">
+        <h1 class="text-center">Lokasi Kami</h1> 
+
+        <br>
+
+        <center>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.0834005610534!2d107.6342279791828!3d-6.969913299685282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e9574579163d%3A0xe3afbb399ab93206!2sKost%20T-2!5e0!3m2!1sen!2sid!4v1665909925327!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </center>
+    </div>
+</section>
+
+<br><br><br>
+  <br><br><br>
+
+        
+    <section id="Jasa Kami">
+      <br><br>
+      <h1 class="text-center"> Jasa Kami </h1>
+      <br><br>
+      <div class="row" align="center">
+        <br><br>
+        <div class="col-sm-4">
+          <div class="card" style="width: 18rem;">
+            <img src="gambar2.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">PAKET KEREN</h5>
+              <p class="card-text">melakukan cukur serta creambath dengan harga 35k</p>
+              <a href="formulir.html" class="btn btn-primary">BOOKING</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card" style="width: 18rem;">
+            <img src="gambar3.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">PAKET MANTAP</h5>
+              <p class="card-text">melakukan cukur rambut serta melakukan shaving dengan harga 50k</p>
+              <a href="formulir.html" class="btn btn-primary">BOOKING</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card" style="width: 18rem;">
+            <img src="gambar1.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">PAKET JAGO</h5>
+              <p class="card-text">melakukan pewarnaan rambut dengan harga 100k</p>
+              <a href="formulir.html" class="btn btn-primary">BOOKING</a>
+            </div>
+          </div>
+        </div>
+    </section>
+
+</body>
+</html>
